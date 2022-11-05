36fe7b3682e5186e007a609dc93c8e0b42ccf781
vũ nguyễn tiến <nguyentienvu4897@gmail.com>
2022-11-05 09:59:59 +0700

    fixed

--- resources/views/search_result.blade.php
+++ resources/views/search_result.blade.php
@@ -9,256 +9,174 @@ Kết quả tìm kiếm
 {{url(''.$setting->logo)}}
 @endsection
 @section('css')
+<link rel="preload" as="style" type="text/css" href="{{asset('frontend/css/evo-collections.scss.css')}}" />
+<link href="{{asset('frontend/css/evo-collections.scss.css')}}" rel="stylesheet" type="text/css" />
 @endsection
 @section('js')
-<link rel="preload" as="style" type="text/css" href="{{asset('frontend/css/collections.scss.css')}}" />
-<link href="{{asset('frontend/css/collections.scss.css')}}" rel="stylesheet" type="text/css" />
-<link href="{{asset('frontend/css/jquery-ui-min.css')}}" rel="stylesheet" type="text/css" />
-<script>
-var swiper = new Swiper('.swiper-cate-col', {
-	slidesPerView: 5,
-	spaceBetween: 10,
-	grabCursor: false,
-	scrollbar: {
-		el: ".swiper-cate-col .swiper-scrollbar",
-		hide: true,
-	},
-	breakpoints: {
-		1199: {
-			slidesPerView: 5,
-			spaceBetween: 10,
-			touchRatio: 1,
-			loop: false,
-		},
-		991: {
-			slidesPerView: 5,
-			spaceBetween: 10,
-			touchRatio: 1,
-			loop: false,
-		},
-		767: {
-			slidesPerView: 4,
-			slidesPerGroup: 2,
-			spaceBetween: 20,
-			touchRatio: 1,
-			loop: false,
-		},
-		480: {
-			slidesPerGroup: 3,
-			slidesPerView: 2,
-			spaceBetween: 20,
-			touchRatio: 1,
-			loop: false,
-		},
-		320: {
-			slidesPerView: 2,
-			slidesPerGroup: 2,
-			spaceBetween: 10,
-			touchRatio: 1,
-			loop: false,
-		}
-	}
-});
-</script>
 @endsection
 @section('content')
 <section class="bread-crumb">
 	<div class="container">
-	<div class="row">
-		<div class="col-12 a-left">
-			<ul class="breadcrumb" >
-				<li class="home">
-				<a  href="{{route('home')}}" ><span >Trang chủ</span></a>						
-				<span class="mr_lr">&nbsp;/&nbsp;</span>
-				</li>
-				<li><strong ><span> Đã tìm thấy {{$countproduct}} kết quả phù hợp</span></strong></li>
-			</ul>
-		</div>
-	</div>
+	<ul class="breadcrumb" itemscope itemtype="https://schema.org/BreadcrumbList">
+		<li class="home" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+			<a itemprop="item" href="{{route('home')}}" title="Trang chủ">
+				<span itemprop="name">Trang chủ</span>
+				<meta itemprop="position" content="1" />
+			</a>
+		</li>
+		<li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+			<strong>
+				<span itemprop="name">Kết quả tìm kiếm</span>
+				<meta itemprop="position" content="2" />
+			</strong>
+		</li>
+	</ul>
 	</div>
 </section>
-<div class="container cate-content">
-	<h1 class="title-collection">
-	<span>
-		Đã tìm thấy {{$countproduct}} kết quả phù hợp
-	</span>
-	</h1>
+<div class="container">
+	<div class="main_container collection margin-bottom-5 filter-url" data-url="{{route('filterProduct')}}">
+	<h1 class="col-title">Đã tìm thấy {{$countproduct}} kết quả phù hợp</h1>
+	@if (isset($cate_id))
+		<input type="text" name="cate_id" value="{{$cate_id}}" hidden>
+	@endif
+	@if (isset($type_id))
+		<input type="text" name="type_id" value="{{$type_id}}" hidden>
+	@endif
 	<div class="row">
-	<aside class="sidebar col-lg-12 col-md-12 col-sm-12 col-12">
-		<div class="aside-category-sort">
-			<aside class="aside-item aside-category">
-				<div class="aside-content">
-				<div class="swiper-cate-col swiper-container">
-					<div class="swiper-wrapper">
-						@foreach ($categoryhome as $cate)
-							<div class="swiper-slide thumb_s">
-							<a class="image" href="{{route('allListProCate',['cate'=>$cate->slug])}}" title="{{languageName($cate->name)}}">
-							<span>{{languageName($cate->name)}}</span>
-							</a>
-							</div>
-						@endforeach
-					</div>
-					<div class="swiper-scrollbar"></div>
+		<div class="col-lg-9 col-md-12">
+			<div class="category-products products category-products-grids clearfix">
+				<div class="sort-cate clearfix">
+				<div class="sort-cate-left">
+					<h3>
+						<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+							width="12px" height="12px" viewBox="0 0 97.761 97.762" style="enable-background:new 0 0 97.761 97.762;"
+							xml:space="preserve">
+							<path d="M42.761,65.596H34.75V2c0-1.105-0.896-2-2-2H16.62c-1.104,0-2,0.895-2,2v63.596H6.609c-0.77,0-1.472,0.443-1.804,1.137
+							c-0.333,0.695-0.237,1.519,0.246,2.117l18.076,26.955c0.38,0.473,0.953,0.746,1.558,0.746s1.178-0.273,1.558-0.746L44.319,68.85
+							c0.482-0.6,0.578-1.422,0.246-2.117C44.233,66.039,43.531,65.596,42.761,65.596z"/>
+							<path d="M93.04,95.098L79.71,57.324c-0.282-0.799-1.038-1.334-1.887-1.334h-3.86c-0.107,0-0.213,0.008-0.318,0.024
+							c-0.104-0.018-0.21-0.024-0.318-0.024h-3.76c-0.849,0-1.604,0.535-1.887,1.336L54.403,95.1c-0.215,0.611-0.12,1.289,0.255,1.818
+							s0.983,0.844,1.633,0.844h5.773c0.88,0,1.657-0.574,1.913-1.416l2.536-8.324h14.419l2.536,8.324
+							c0.256,0.842,1.033,1.416,1.913,1.416h5.771c0.649,0,1.258-0.314,1.633-0.844C93.16,96.387,93.255,95.709,93.04,95.098z
+							M68.905,80.066c2.398-7.77,4.021-13.166,4.82-16.041l4.928,16.041H68.905z"/>
+							<path d="M87.297,34.053H69.479L88.407,6.848c0.233-0.336,0.358-0.734,0.358-1.143V2.289c0-1.104-0.896-2-2-2H60.694
+							c-1.104,0-2,0.896-2,2v3.844c0,1.105,0.896,2,2,2h16.782L58.522,35.309c-0.233,0.336-0.358,0.734-0.358,1.146v3.441
+							c0,1.105,0.896,2,2,2h27.135c1.104,0,2-0.895,2-2v-3.842C89.297,34.947,88.402,34.053,87.297,34.053z"/>
+						</svg>
+						Xếp theo
+					</h3>
+					<ul>
+						<li class="btn-quick-sort alpha-asc">
+							<a href="javascript:;" onclick="sortby('alpha-asc')" title="Tên A-Z"><i></i>Tên A-Z</a>
+						</li>
+						<li class="btn-quick-sort alpha-desc">
+							<a href="javascript:;" onclick="sortby('alpha-desc')" title="Tên Z-A"><i></i>Tên Z-A</a>
+						</li>
+						<li class="btn-quick-sort position-desc">
+							<a href="javascript:;" onclick="sortby('created-desc')" title="Hàng mới"><i></i>Hàng mới</a>
+						</li>
+						<li class="btn-quick-sort price-asc">
+							<a href="javascript:;" onclick="sortby('price-asc')" title="Giá thấp đến cao"><i></i>Giá thấp đến cao</a>
+						</li>
+						<li class="btn-quick-sort price-desc">
+							<a href="javascript:;" onclick="sortby('price-desc')" title="Giá cao xuống thấp"><i></i>Giá cao xuống thấp</a>
+						</li>
+					</ul>
+				</div>
+				<div class="evo-filter d-sm-inline-block d-lg-none">
+					<a class="btn btn-outline evo-btn-filter" title="Lọc">
+						Lọc
+						<svg class="svg-filter ml-2" xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12">
+							<path fill="#666" fill-rule="nonzero" d="M11.214 0H.504a.503.503 0 0 0-.448.273.51.51 0 0 0 .04.53l3.923 5.522.004.005c.143.193.22.425.22.665v4.501a.5.5 0 0 0 .699.464l2.205-.84a.477.477 0 0 0 .328-.47V6.995c0-.24.078-.472.22-.665l.004-.005L11.623.803a.509.509 0 0 0 .04-.53.503.503 0 0 0-.449-.273z"></path>
+						</svg>
+					</a>
 				</div>
 				</div>
-			</aside>
-		</div>		
-	</aside>
-	<section class="main_container collection col-xl-9 col-lg-9 col-md-12 col-sm-12 col-12">
-		<div class="category-products products category-products-grids clearfix">
-			<section class="products-view products-view-grid row">
+				<section class="products-view products-view-grid row">
 				@foreach ($resultPro as $product)
-				<div class="col-lg-4 col-md-4 col-sm-6 col-6">
+				<div class="col-lg-3 col-md-3 col-sm-4 col-6">
 					@include('layouts.product.item', ['product'=>$product])
 				</div>
 				@endforeach
-			</section>
-			{{-- <div class="row">
-				<div class="col-lg-12 col-sm-12 col-12 margin-top-20 fix-pag">
-				<div class="paginate">
-					{{$list->links()}}
+				<div class="text-xs-right text-center pagging-css margin-top-20">
+					<nav class="text-center">
+						{{-- {{$list->links()}} --}}
+					</nav>
 				</div>
-				</div>
-			</div> --}}
-		</div>
-	</section>
-	<aside class="left-content col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12">
-		<aside class="aside-item sidebar-category collection-category margin-bottom-0">
-			<div class="aside-content aside-content-menu">
-				<nav class="nav-category navbar-toggleable-md">
-				<ul class="nav navbar-pills">
-					<li class="nav-item">
-						<a class="nav-link" href="{{route('home')}}">Trang chủ</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="">Sale</a>
-					</li>
-					<li class="nav-item">
-						<a href="{{route('allProduct')}}" class="nav-link">Sản phẩm</a>
-						<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-down" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="fa-plus svg-down svg-inline--fa fa-chevron-down fa-w-14">
-							<path fill="currentColor" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z" class=""></path>
-						</svg>
-						<ul class="dropdown-menu ul_1">
-							@foreach ($categoryhome as $cate)
-							<li class="dropdown-submenu nav-item">
-								<a class="nav-link" href="{{route('allListProCate', ['cate'=>$cate->slug])}}">{{languageName($cate->name)}}
-								</a>
-								@if (count($cate->typeCate) > 0)
-									<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-down" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="fa-plus svg-down svg-inline--fa fa-chevron-down fa-w-14">
-										<path fill="currentColor" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z" class=""></path>
-									</svg>
-									<ul class="dropdown-menu ul_2">
-										@foreach ($cate->typeCate as $type)
-										<li class="nav-item lv3">
-											<a class="nav-link" href="{{route('allListProType', ['cate'=>$type->cate_slug, 'type'=>$type->slug])}}">{{languageName($type->name)}}
-											</a>
-										</li>
-										@endforeach
-									</ul>
-								@endif
-							</li>
-							@endforeach
-						</ul>
-					</li>
-					<li class="nav-item">
-						<a href="{{route('allListBlog')}}" class="nav-link">Tin tức</a>
-						<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-down" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="fa-plus svg-down svg-inline--fa fa-chevron-down fa-w-14">
-							<path fill="currentColor" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z" class=""></path>
-						</svg>
-						<ul class="dropdown-menu ul_1">
-							@foreach ($blogCate as $cate)
-							<li class="dropdown-submenu nav-item">
-								<a class="nav-link" href="{{route('listCateBlog', ['slug'=>$cate->slug])}}">{{languageName($cate->name)}}
-								</a>
-								@if (count($cate->typeCate) > 0)
-									<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-down" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="fa-plus svg-down svg-inline--fa fa-chevron-down fa-w-14">
-										<path fill="currentColor" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z" class=""></path>
-									</svg>
-									<ul class="dropdown-menu ul_2">
-										@foreach ($cate->typeCate as $type)
-										<li class="nav-item lv3">
-											<a class="nav-link" href="{{route('listTypeBlog', ['slug'=>$type->slug])}}">{{languageName($type->name)}}
-											</a>
-										</li>
-										@endforeach
-									</ul>
-								@endif
-							</li>
-							@endforeach
-						</ul>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="{{route('lienHe')}}">Liên hệ</a>
-					</li>
-				</ul>
-				</nav>
+				</section>
 			</div>
-		</aside>
-		<div class="aside-filter clearfix">
-			<div class="aside-hidden-mobile">
-				<div class="filter-containers">
-				<div class="filter-container__selected-filter" style="display: none;">
-					<div class="filter-container__selected-filter-list clearfix">
-						<div class="filter-container__selected-filter-header">
-							<span class="filter-container__selected-filter-header-title">Bạn chọn</span>
-						</div>
-						<ul>
-						</ul>
-						<div class="filter-container__selected-filter-footer d-none">
-							<a href="javascript:void(0)" onclick="clearAllFiltered()" class="filter-container__clear-all" title="Bỏ hết">Bỏ hết</a>
-						</div>
-					</div>
-				</div>
-				</div>
-				<div class="clearfix"></div>
-				<div class="filter-container">
-				<aside class="aside-item filter-vendor">
-					<div class="aside-title">Thương hiệu 
-					</div>
-					<div class="aside-content filter-group">
-						<div class="search-collection">
-							<input type="text" placeholder="Tìm kiếm" class="form-control filter-vendor-list" onkeyup="filterItemInList(jQuery('.filter-vendor-list'))">
-							<div class="button_search">
-							<button type="submit" class="btn btn-default">
-								<svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
-									<path fill-rule="evenodd" clip-rule="evenodd" d="M14.4057 12.4769C16.0756 10.1337 15.8595 6.85958 13.7572 4.75736C11.4141 2.41421 7.61509 2.41421 5.27195 4.75736C2.9288 7.1005 2.9288 10.8995 5.27195 13.2426C7.37417 15.3449 10.6483 15.561 12.9915 13.8911L18.707 19.6066L20.1212 18.1924L14.4057 12.4769ZM12.343 6.17157C13.9051 7.73367 13.9051 10.2663 12.343 11.8284C10.7809 13.3905 8.24826 13.3905 6.68616 11.8284C5.12406 10.2663 5.12406 7.73367 6.68616 6.17157C8.24826 4.60948 10.7809 4.60948 12.343 6.17157Z" fill="#333333"/>
-								</svg>
-							</button>
+		</div>
+		<div class="col-lg-3 col-md-12 left-content">
+			<div class="evo-sidebar-pro">
+				<div class="aside-filter clearfix">
+				<div class="heading">Lọc</div>
+				<div class="aside-hidden-mobile">
+					<div class="filter-container">
+						<div class="filter-containers d-none">
+							<div class="filter-container__selected-filter" style="display: none;">
+							<div class="filter-container__selected-filter-list clearfix">
+								<ul>
+								</ul>
+								<a href="javascript:void(0)" onclick="clearAllFiltered()" class="filter-container__clear-all" title="Bỏ hết">Bỏ hết</a>
+							</div>
 							</div>
 						</div>
-						<ul class="filter-vendor">
-							@foreach ($productBrands as $brand)
-							<li class="filter-item filter-item--check-box filter-item--green ">
-								<label data-filter="chanel" for="filter-chanel" class="chanel">
-								<input type="checkbox" id="filter-chanel" onchange="toggleFilter(this)"  data-group="Hãng" data-field="vendor.filter_key" data-text="Chanel" value="(&#34;Chanel&#34;)" data-operator="OR">
+						<div class="clearfix"></div>
+						<aside class="aside-item filter-price">
+							<div class="aside-title">Giá sản phẩm <span class="svg svg1 collapsible-plus"></span></div>
+							<ul class="aside-content filter-group">
+							<li class="filter-item filter-item--check-box filter-item--green">
+								<label for="filter-duoi-100-000d">
+								<input type="checkbox" id="filter-duoi-100-000d" onchange="toggleFilter(this);" data-group="Khoảng giá" data-field="price_min" data-text="Dưới 100.000đ" value="(<100000)" data-operator="OR">
 								<i class="fa"></i>
-								<span>{{$brand->name}}</span>
+								Giá dưới 100.000đ
 								</label>
 							</li>
-							@endforeach
-						</ul>
+							<li class="filter-item filter-item--check-box filter-item--green">
+								<label for="filter-100-000d-200-000d">
+								<input type="checkbox" id="filter-100-000d-200-000d" onchange="toggleFilter(this)" data-group="Khoảng giá" data-field="price_min" data-text="100.000đ - 200.000đ" value="(>=100000 AND <=200000)" data-operator="OR">
+								<i class="fa"></i>
+								100.000đ - 200.000đ							
+								</label>
+							</li>
+							<li class="filter-item filter-item--check-box filter-item--green">
+								<label for="filter-200-000d-300-000d">
+								<input type="checkbox" id="filter-200-000d-300-000d" onchange="toggleFilter(this)" data-group="Khoảng giá" data-field="price_min" data-text="200.000đ - 300.000đ" value="(>=200000 AND <=300000)" data-operator="OR">
+								<i class="fa"></i>
+								200.000đ - 300.000đ							
+								</label>
+							</li>
+							<li class="filter-item filter-item--check-box filter-item--green">
+								<label for="filter-300-000d-500-000d">
+								<input type="checkbox" id="filter-300-000d-500-000d" onchange="toggleFilter(this)" data-group="Khoảng giá" data-field="price_min" data-text="300.000đ - 500.000đ" value="(>=300000 AND <=500000)" data-operator="OR">
+								<i class="fa"></i>
+								300.000đ - 500.000đ							
+								</label>
+							</li>
+							<li class="filter-item filter-item--check-box filter-item--green">
+								<label for="filter-500-000d-1-000-000d">
+								<input type="checkbox" id="filter-500-000d-1-000-000d" onchange="toggleFilter(this)" data-group="Khoảng giá" data-field="price_min" data-text="500.000đ - 1.000.000đ" value="(>=500000 AND <=1000000)" data-operator="OR">
+								<i class="fa"></i>
+								500.000đ - 1.000.000đ							
+								</label>
+							</li>
+							<li class="filter-item filter-item--check-box filter-item--green">
+								<label for="filter-tren1-000-000d">
+								<input type="checkbox" id="filter-tren1-000-000d" onchange="toggleFilter(this)" data-group="Khoảng giá" data-field="price_min" data-text="Trên 1.000.000đ" value="(>1000000)" data-operator="OR">
+								<i class="fa"></i>
+								Giá trên 1.000.000đ
+								</label>
+							</li>
+							</ul>
+						</aside>
 					</div>
-				</aside>
-				<aside class="aside-item filter-price">
-					<div class="aside-title">Giá 
-					</div>
-					<div class="aside-content filter-groupi filter-ui-slider">
-						<div id='start'><input value="0"></div>
-						<div id='stop'><input value="10.000.000₫"></div>
-						<div id='slider'></div>
-						<span class="price_val"></span>
-						<a id="old-value" href="javascript:;"></a>
-						<a id="filter-value" class="btn btn-primary" href="javascript:;" onclick="_toggleFilterdqdt(this);"  data-value="(>-1 AND <10000001)" >Lọc giá</a>
-					</div>
-				</aside>
+				</div>
 				</div>
 			</div>
 		</div>
-	</aside>
+	</div>
 	</div>
 </div>
-<div id="open-filters" class="open-filters d-lg-none d-block">
-	<span class="fter">
-	</span>
+<div class="product_recent product-page-viewed">
 </div>
 @endsection
